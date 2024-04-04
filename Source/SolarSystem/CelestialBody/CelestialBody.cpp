3a28e92d6012a3a8804da37668431e2b1345b901
Felix Wahl <felix.wahl@live.de>
2024-04-04 16:07:23 +0200

    Added CelestialBody

---
+++ Source/SolarSystem/CelestialBody/CelestialBody.cpp
@@ -0,0 +1,81 @@
+﻿// Author (c) 2024 Felix Wahl (https://github.com/goldbarth). Provided under the MIT License. Full text: https://opensource.org/licenses/MIT
+
+#include "CelestialBody.h"
+
+#include "SolarSystem/GameModes/DemoOrbitSimulation_GameMode.h"
+#include "SolarSystem/Orbit/ACelestialBodyRegistry.h"
+#include "SolarSystem/Structs/Universe.h"
+#include "../Defines/Debug.h"
+
+
+ACelestialBody::ACelestialBody()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	SetMeshComponent();
+	SetRadius();
+}
+
+void ACelestialBody::BeginPlay()
+{
+	Super::BeginPlay();
+	SetCurrentVelocity(InitialVelocity);
+	SetRadius();
+	MassCalculation();
+	
+	FTimerHandle TimerHandle;
+	GetWorldTimerManager().SetTimer(TimerHandle, this, &ACelestialBody::AddObjectToRegistry, 0.5f, false);
+}
+
+void ACelestialBody::SetMeshComponent()
+{
+	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComponent"));
+	RootComponent = MeshComponent;
+	MeshComponent->SetSimulatePhysics(true);
+	MeshComponent->SetEnableGravity(false);
+}
+
+void ACelestialBody::SetMass(const float& NewMass)
+{
+	Mass = NewMass;
+	MeshComponent->SetMassOverrideInKg(NAME_None, Mass, true);
+	GravityCalculation();
+}
+
+void ACelestialBody::MassCalculation()
+{
+	Mass = Radius * Radius / FUniverse::GravitationalConstant;
+	MeshComponent->SetMassOverrideInKg(NAME_None, Mass, true);
+	
+}
+
+void ACelestialBody::UpdateVelocity(const FVector& Acceleration, const float& TimeStep)
+{
+	CurrentVelocity += Acceleration * TimeStep;
+}
+
+void ACelestialBody::UpdatePosition(const float& TimeStep) const
+{
+	MeshComponent->SetPhysicsLinearVelocity(CurrentVelocity * TimeStep);
+}
+
+void ACelestialBody::AddObjectToRegistry()
+{
+	ADemoOrbitSimulation_GameMode* GameMode = Cast<ADemoOrbitSimulation_GameMode>(GetWorld()->GetAuthGameMode());
+	if (GameMode)
+	{
+		ACelestialBodyRegistry* Registry = GameMode->GetCelestialBodyRegistry();
+		if (Registry)
+		{
+			Registry->OnCelestialBodyAdded.Broadcast(this);
+		}
+		else
+		{
+			LOG_ERROR_F("Failed to get CelestialObjectManager from Game Mode! @ %s", *GetName());
+		}
+	}
+	else
+	{
+		LOG_ERROR_F("Failed to cast Game Mode! @ %s", *GetName());
+	}
+}
+
---
+++ Source/SolarSystem/CelestialBody/CelestialBody.h
@@ -0,0 +1,58 @@
+﻿// Author (c) 2024 Felix Wahl (https://github.com/goldbarth). Provided under the MIT License. Full text: https://opensource.org/licenses/MIT
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CelestialBody.generated.h"
+
+UCLASS()
+class SOLARSYSTEM_API ACelestialBody : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ACelestialBody();
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* MeshComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Celestial Body")
+	float Mass;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Celestial Body")
+	float Radius;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Celestial Body")
+	FVector InitialVelocity;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Celestial Body")
+	FVector CurrentVelocity;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Debug Options")
+	mutable FLinearColor LineColor;
+	
+public:
+	float GetMass() const { return Mass; }
+	void SetMass(const float& NewMass);
+
+	float GetRadius() const { return Radius; }
+	void SetRadius() { Radius = MeshComponent->Bounds.SphereRadius; }
+
+	FVector GetInitialVelocity() const { return InitialVelocity; }
+	void SetCurrentVelocity(const FVector& NewVelocity) { CurrentVelocity = NewVelocity; }
+
+	FLinearColor GetLineColor() const { return LineColor; }
+
+	void UpdatePosition(const float& TimeStep) const;
+	void UpdateVelocity(const FVector& Acceleration, const float& TimeStep);
+
+private:
+	void SetMeshComponent();
+	void MassCalculation();
+	void GravityCalculation();
+	void AddObjectToRegistry();
+};
