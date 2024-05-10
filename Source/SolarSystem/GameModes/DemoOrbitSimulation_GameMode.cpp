f04f0ec3e394325e922fb32cbb4fce215bfd221a
Felix Wahl <felix.wahl@live.de>
2024-05-10 14:27:00 +0200

    Added the Game Mode class

---
+++ Source/SolarSystem/GameModes/DemoOrbitSimulation_GameMode.cpp
@@ -0,0 +1,39 @@
+﻿// Author (c) 2024 Felix Wahl (https://github.com/goldbarth). Provided under the MIT License. Full text: https://opensource.org/licenses/MIT
+
+
+#include "DemoOrbitSimulation_GameMode.h"
+
+#include "../Defines/Debug.h"
+
+void ADemoOrbitSimulation_GameMode::StartPlay()
+{
+	Super::StartPlay();
+	
+	InitializeSimulation();
+}
+
+void ADemoOrbitSimulation_GameMode::InitializeSimulation()
+{
+	UWorld* World = GetWorld();
+	if (World)
+	{
+		CelestialBodyRegistry = World->SpawnActor<ACelestialBodyRegistry>();
+		if (!CelestialBodyRegistry)
+		{
+			LOG_WARNING("Failed to create CelestialObjectManager! (DemoOrbitSimulation_GameMode)")
+		}
+		else
+		{
+			LOG_DISPLAY("Created CelestialObjectManager! (DemoOrbitSimulation_GameMode)")
+		}
+		OrbitSimulation = World->SpawnActor<AOrbitSimulation>();
+		if (!OrbitSimulation)
+		{
+			LOG_ERROR("Failed to create OrbitSimulation! (DemoOrbitSimulation_GameMode)")
+		}
+		else
+		{
+			LOG_DISPLAY("Created OrbitSimulation! (DemoOrbitSimulation_GameMode)")
+		}
+	}
+}
---
+++ Source/SolarSystem/GameModes/DemoOrbitSimulation_GameMode.h
@@ -0,0 +1,36 @@
+﻿// Author (c) 2024 Felix Wahl (https://github.com/goldbarth). Provided under the MIT License. Full text: https://opensource.org/licenses/MIT
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SolarSystem/Orbit/OrbitSimulation.h"
+#include "SolarSystem/Orbit/ACelestialBodyRegistry.h"
+#include "DemoOrbitSimulation_GameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SOLARSYSTEM_API ADemoOrbitSimulation_GameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Celestial Objects")
+	AOrbitSimulation* OrbitSimulation;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Celestial Objects")
+	ACelestialBodyRegistry* CelestialBodyRegistry;
+
+
+public:
+	AOrbitSimulation* GetOrbitSimulation() const { return OrbitSimulation; }
+	ACelestialBodyRegistry* GetCelestialBodyRegistry() const { return CelestialBodyRegistry; }
+
+	virtual void StartPlay() override;
+	
+private:
+	void InitializeSimulation();
+	
+};
